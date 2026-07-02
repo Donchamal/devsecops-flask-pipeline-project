85268575d38220d46cc3af4dd8634db9d44b3f8b
Chamal Probodahan Weerasingha <chamalprobodahan@gmail.com>
2026-07-02 20:33:51 +0530

    Added Flask login authentication

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+# Demo credentials
+USERNAME = "admin"
+PASSWORD = "admin123"
+
 
 @app.route("/")
 def login():
     return render_template("login.html")
 
+
+@app.route("/login", methods=["POST"])
+def authenticate():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == USERNAME and password == PASSWORD:
+        return redirect(url_for("dashboard"))
+
+    return "Invalid Username or Password"
+
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
